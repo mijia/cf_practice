f9468c64f24a016c679fd7792bbb3895bb4ccd60
Jia Mi <winters.mi@gmail.com>
2015-07-14 18:58:46 +0800

    Solve 22

---
+++ euler/0022.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"strings"
+)
+
+func main() {
+	data, err := ioutil.ReadFile("p022_names.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	words := strings.Split(string(data), ",")
+	for i := range words {
+		words[i] = words[i][1 : len(words[i])-1]
+	}
+	sort.Strings(words)
+
+	sum := 0
+	for i, word := range words {
+		sum += nameScore(i+1, word)
+	}
+	fmt.Println(sum)
+}
+
+func nameScore(index int, word string) int {
+	data := []byte(word)
+	score := 0
+	for _, ch := range data {
+		score += int(ch - 64)
+	}
+	return index * score
+}
